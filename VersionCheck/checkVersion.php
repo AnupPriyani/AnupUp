46850392976a958168ded6b1c61300ae228222b9
AnupPriyani <109275195+AnupPriyani@users.noreply.github.com>
2022-07-14 14:02:21 +0530

    Add files via upload

---
+++ VersionCheck/checkVersion.php
@@ -0,0 +1,24 @@
+<?php
+public function getAndroidVersion(string $storeUrl): string
+{
+    $dom = new DOMDocument();
+    $dom->loadHTML(file_get_contents($storeUrl));
+    libxml_use_internal_errors(false);
+    $elements = $dom->getElementsByTagName('span');
+
+    $depth = 0;
+    foreach ($elements as $element) {
+        foreach ($element->attributes as $attr) {
+            if ($attr->nodeName === 'class' && $attr->nodeValue === 'htlgb') {
+                $depth++;
+                if ($depth === 7) {
+                    return preg_replace('/[^0-9.]/', '', $element->nodeValue);
+                    break 2;
+                }
+            }
+        }
+    }
+}
+
+/*Write the scripts to extract the latitude and longitude from your URL*/
+?>
